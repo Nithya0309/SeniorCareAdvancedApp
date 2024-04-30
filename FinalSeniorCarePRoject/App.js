7485ee701d961e1a970fadfba395bf0df99b5253
Nithya0309 <126447688+Nithya0309@users.noreply.github.com>
2024-04-30 14:08:18 +0530

    Add files via upload

---
+++ FinalSeniorCarePRoject/App.js
@@ -0,0 +1,39 @@
+import Login from "./Pages/LoginPage";
+import Home from "./Pages/HomePage";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route,Routes } from "react-router-dom";
+import Dashboard from "./Pages/AdminDashboard";
+import Navbar from "./Components/NavBar";
+import UserDashboard from "./Pages/UserDashboard";
+import Footer from "./Components/Footer";
+import MenPage from "./Pages/Men";
+import WomenPage from "./Pages/Women";
+import KidsPage from "./Pages/Kids";
+import SearchPage from "./Pages/searchPage";
+import Cart from "./Pages/Cart";
+import AccessoriesPage from "./Pages/Access";
+import RazorpayComponent from "./Razorpay";
+function App() {
+  return (
+    <>
+    
+      <Routes>
+        <Route path="/" element={<><Navbar/><Footer/></>} >
+        <Route index element={<Home />} />
+        <Route path="/profile" element={<UserDashboard />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/men" element={<MenPage />} />
+        <Route path="/women" element={<WomenPage />} />
+        <Route path="/kids" element={<KidsPage />} />
+        <Route path="/accessories" element={<AccessoriesPage />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/AdminDashBoard" element={<Dashboard />} />
+        <Route path="/Razorpay" element={<RazorpayComponent />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
